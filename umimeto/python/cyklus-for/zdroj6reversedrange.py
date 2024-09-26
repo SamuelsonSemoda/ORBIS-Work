b15720ba83992c6d62bd4cb5f448ec1e11800abc
SamuelsonSemoda <samuel.semoda@orbiszlin.cz>
2024-09-26 07:58:40 +0200

    Update zdroj6reversedrange.py

--- umimeto/python/cyklus-for/zdroj6reversedrange.py
+++ umimeto/python/cyklus-for/zdroj6reversedrange.py
@@ -4,4 +4,4 @@ def reverse_numbers(n):
     for i in reversed(range(1, n+1)): #1 je starting point, n+1 je v jakém range - v mém případě nechci nulu
         print(i)
 
-  #výsledek print(6) = 6,5,4,3,2,1
+  #výsledek reverse_numbers(6) = 6,5,4,3,2,1
