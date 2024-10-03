3623de2bd5c402584574cf21d3b9ef0ecbc2025e
SamuelsonSemoda <samuel.semoda@orbiszlin.cz>
2024-10-03 08:49:04 +0200

    Delete umimeto/python/cyklus-while.py

--- umimeto/python/cyklus-while.py
+++
@@ -1,7 +0,0 @@
-#Napište funkci repeated_division(n), která opakovaně vypisuje a dělí zadané kladné číslo n na poloviny, dokud je větší než nula (viz ukázka). 
-#Použijte celočíselné dělení, které v Pythonu zapisujeme pomocí dvou lomítek (//).
-
-def repeated_division(n):
-    while n > 0:
-    	print(n)
-        n //= 2
