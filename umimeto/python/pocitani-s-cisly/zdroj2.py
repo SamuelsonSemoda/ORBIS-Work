12a419a77a44a136d57952b76a8e3c52da741f38
SamuelsonSemoda <samuel.semoda@orbiszlin.cz>
2024-10-24 08:35:06 +0200

    Update zdroj2.py

---
+++ umimeto/python/pocitani-s-cisly/zdroj2.py
@@ -0,0 +1,8 @@
+#Napište funkci divisors(n), která vypíše popořadě všechny přirozená čísla, která dělí n.
+
+def divisors(n):
+    div = 0
+    while div <= n:
+        div += 1
+		if n % div == 0:
+            print(div)
