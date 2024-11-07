6b6a4bf9afe52edf777101ed1814bed4c0ada577
SamuelsonSemoda <samuel.semoda@orbiszlin.cz>
2024-11-07 08:46:35 +0100

    Update zdroj9.py

--- umimeto/python/pocitani-s-cisly/zdroj9.py
+++ umimeto/python/pocitani-s-cisly/zdroj9.py
@@ -1,7 +1,7 @@
 #Napište funkci numbers_around(n, k), která vypíše seřazeně k menších a větších čísel než n. Místo čísla n vypíše hvězdičku.
 
 def numbers_around(n, k):
-    for i in range(n - k, n + k + 1):
+    for i in range(n - k, n + k + 1): #od n-k to začíná a od n + k + 1 to končí, tedy vlastně děláme kolik čísel vrátí program od daného čísla.
         if i == n:
             print('*', end=' ')
         else:
