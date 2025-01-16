7a55aef0e425c5190e7dbf2fb5a5dd101119d686
Redidloo <samuel.semoda@orbiszlin.cz>
2025-01-16 08:54:31 +0100

    Update zdroj2.py

--- umimeto/python/retezce-teszi/zdroj2.py
+++ umimeto/python/retezce-teszi/zdroj2.py
@@ -1,10 +1,8 @@
 #Napište funkci common_prefix(text1, text2), která vrátí společný začátek řetězců text1 a text2.
 
 def common_prefix(text1, text2):
-    prefix = ""
-    for char in range(min(len(text1), len(text2))):
-        if text1[char] == text2[char]:
-            prefix += text1[char]
-        else:
-            break
-    return prefix
+    myText = str()
+    for pismeno in range(min(len(text1), len(text2))):
+        if text1[pismeno] == text2[pismeno]:
+            myText += (text1[pismeno])
+    return myText
